d77194da2e20ceef97459d6c5fbc049a3aad9275
wangce <949804781@qq.com>
2017-03-09 14:05:32 +0800

    添加快速排序

---
+++ code/quicksort/quicksort.cpp
@@ -0,0 +1,44 @@
+# include <stdio.h>
+
+/**
+ * 第二个参数表示第一个参数的下标
+ * 第三个参数表示最后一个参数的下标
+ */
+void quickSort(int *, int, int);
+
+int findPos(int *a, int low, int high);
+
+void main() {
+	int a[6] = {2, 1, 0, 5, 4, 3};
+	quickSort(a, 0, 5);
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
+void quickSort(int *a, int low, int high) {
+	if (low >= high) return;
+	int pos;
+	if (low < high) {
+		pos = findPos(a, low, high);
+		quickSort(a, low, pos-1);
+		quickSort(a, pos+1, high);
+	}
+}
+
+int findPos(int *a, int low, int high) {
+	int val = a[low];
+	while (low < high) {
+		while (low < high && a[high] >= val) {
+			high --;
+		}
+		a[low] = a[high];
+		while (low <high && a[low] <= val) {
+			low++;
+		}
+		a[high] = a[low];
+	}
+	a[low] = val;
+	return low; // 或return high;
+}
